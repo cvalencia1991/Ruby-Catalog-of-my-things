92d4345a4193c9521a4e0df74edf6aac82bd966e
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-10 20:47:10 +0500

    Define a main entry point

--- main.rb
+++ main.rb
@@ -1,42 +1,9 @@
-# require_relative 'app'
+require_relative 'app'
 
-class Interface
-  @welcome = 'Welcome to the Cataloge App!', ''
-  def interface; end
+class Main                                                                      
+  puts "\nWelcome to the Cataloge App! ⭐ 📝 \n"
+  app = App.new
+  app.show_menu
 end
 
-# class Main
-#   def initialize
-#     @app = App.new
-#     @interface = Interface.new
-#   end
-
-#   def select
-#     option = gets.chomp.to_i
-#     case option
-#     when 1..6
-#       @app.select(option)
-#       run
-#     when 7
-#       @app.save_data
-#       puts 'Thank you for using this App!'
-#     else
-#       puts 'Incorrect Input: Option does not exist'
-#       run
-#     end
-#   end
-
-#   def start
-#     @app.load_data
-#     run
-#   end
-
-#   def run
-#     @interface.interface
-#     select
-#   end
-
-#   private :select
-# end
-
-# Main.new.start
+Main.new
