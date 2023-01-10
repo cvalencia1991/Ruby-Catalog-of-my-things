4e389d3f216e7c7b60c4045a3a6a299cc43fa5a6
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-10 20:46:00 +0500

    Allow user to select an option

--- app.rb
+++ app.rb
@@ -14,4 +14,47 @@ class App
     user_choice = gets.chomp.to_i
     select_option(user_choice)
   end
+
+  def select_option(user_choice)
+    case user_choice
+    when 1..6
+      list_items(user_choice)
+    when 7..9
+      add_items(user_choice)
+    when 0
+      exit
+    else
+      invalid_option
+    end
+  end
+
+  def invalid_option
+    puts "\n 🛑 ❌ ❌ Invalid option. Please select a valid input..."
+    show_menu
+  end
+
+  def list_items(user_choice)
+    case user_choice
+    when 1 then # list_all_books
+    when 2 then # list_all_music_albums
+    when 3 then # list_all_games
+    when 4 then # list_all_genres
+    when 5 then # list_all_labels
+    when 6 then # list_authors
+    end
+    show_menu
+  end
+
+  def add_items(user_choice)
+    case user_choice
+    when 7 then # create_book
+    when 8 then # add_music_album
+    when 9 then # add_game
+    end
+    show_menu
+  end
+
+  def exit
+    puts "Thank you for using the app, see you later!👋  \n\n"
+  end
 end
