d01138c89fa08cf9e231174a061e43d52fd226c2
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-10 18:42:46 -0500

    Add Genre testing

---
+++ src/classes/genre_spec.rb
@@ -0,0 +1,22 @@
+require_relative 'genre'
+require 'rspec'
+
+describe Genre do
+  describe 'Genre class' do
+    it 'creates a new Genre name' do
+      genre = Genre.new('cesar')
+      expect(genre.name).to eq('cesar')
+    end
+
+    it 'creates a new Genre name' do
+      genre = Genre.new('cesar')
+      expect(genre.items).to eq([])
+    end
+
+    it 'generate a random id' do
+      genre = Genre.new('cesar')
+      allow(genre).to receive(:rand).and_return(5)
+      expect(genre.generate_id).to eq(5)
+    end
+  end
+end
