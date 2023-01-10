26c6a6693bcee8105080694ae6bb42b35adadefc
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-10 12:34:30 -0500

    Fix RuboCop linters

--- Genre/genre.rb
+++ Genre/genre.rb
@@ -4,7 +4,7 @@ class Genre
   attr_reader :id
   attr_accessor :name, :items
 
-  def initialize(name, id:nil)
+  def initialize(name, id: nil)
     @id = id.nil? ? Random.rand(1..1000) : id
     @name = name.to_s unless name.is_a? String
     @items = []
