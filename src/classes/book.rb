0a5be7c66f50125445dd1004a7c8adfbb5356d23
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-10 23:52:14 +0500

    Set getter and setter for publisher and cover_state

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -2,6 +2,8 @@ require_relative '../items'
 
 class Book < Item
   
+  attr_accessor :publisher, :cover_state
+
   def initialize(date, publisher, cover_state, archived: archived, id: nil)
     super(id, date, archived: archived)
     @publisher = publisher
