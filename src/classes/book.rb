ab728985dc27cbf7772713552b9122c4c0204ec6
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-10 23:51:16 +0500

    Add constructor for Book class

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -1,4 +1,10 @@
 require_relative '../items'
 
 class Book < Item
+  
+  def initialize(date, publisher, cover_state, archived: archived, id: nil)
+    super(id, date, archived: archived)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 end
