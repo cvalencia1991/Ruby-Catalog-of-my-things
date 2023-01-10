4cdca8dd8ddf0454ae0ca60da589614a7c0d42df
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-10 20:52:23 +0500

    Style the app using colorize

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
+require 'colorize'
+
 class App
   def show_menu
-    puts "\nPlease choose an option by entering a number from below 👇: \n\n"
-    puts "[ 1 ] - List all books"
-    puts "[ 2 ] - List all music albums"
-    puts "[ 3 ] - List of games"
-    puts "[ 4 ] - List all genres (e.g 'Comedy', 'Thriller')"
-    puts "[ 5 ] - List all labels (e.g. 'Gift', 'New')"
-    puts "[ 6 ] - List all authors (e.g. 'Stephen King')"
-    puts "[ 7 ] - Add a book"
-    puts "[ 8 ] - Add a music album"
-    puts "[ 9 ] - Add a game"
-    puts "[ 0 ] - Exit \n\n"
+    puts "\nPlease choose an option by entering a number from below 👇: \n\n".yellow.underline
+    puts "[ 1 ] - List all books".cyan
+    puts "[ 2 ] - List all music albums".cyan
+    puts "[ 3 ] - List of games".cyan
+    puts "[ 4 ] - List all genres (e.g 'Comedy', 'Thriller')".cyan
+    puts "[ 5 ] - List all labels (e.g. 'Gift', 'New')".cyan
+    puts "[ 6 ] - List all authors (e.g. 'Stephen King')".cyan
+    puts "[ 7 ] - Add a book".cyan
+    puts "[ 8 ] - Add a music album".cyan
+    puts "[ 9 ] - Add a game".cyan
+    puts "[ 0 ] - Exit \n\n".cyan
     user_choice = gets.chomp.to_i
     select_option(user_choice)
   end
@@ -29,7 +31,7 @@ class App
   end
 
   def invalid_option
-    puts "\n 🛑 ❌ ❌ Invalid option. Please select a valid input..."
+    puts "\n 🛑 ❌ ❌ Invalid option. Please select a valid input...".red.underline
     show_menu
   end
 
@@ -55,6 +57,6 @@ class App
   end
 
   def exit
-    puts "Thank you for using the app, see you later!👋  \n\n"
+    puts "Thank you for using the app, see you later!👋  \n\n".blue
   end
 end
