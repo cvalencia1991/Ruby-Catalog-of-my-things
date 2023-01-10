0d60c01f1ec95fa6f22300ccd60f921fed468cdc
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-10 20:44:53 +0500

    Present user a list of options

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 class App
-  def initialize(_notemptyclass)
-    @notemptyclass
+  def show_menu
+    puts "\nPlease choose an option by entering a number from below 👇: \n\n"
+    puts "[ 1 ] - List all books"
+    puts "[ 2 ] - List all music albums"
+    puts "[ 3 ] - List of games"
+    puts "[ 4 ] - List all genres (e.g 'Comedy', 'Thriller')"
+    puts "[ 5 ] - List all labels (e.g. 'Gift', 'New')"
+    puts "[ 6 ] - List all authors (e.g. 'Stephen King')"
+    puts "[ 7 ] - Add a book"
+    puts "[ 8 ] - Add a music album"
+    puts "[ 9 ] - Add a game"
+    puts "[ 0 ] - Exit \n\n"
+    user_choice = gets.chomp.to_i
+    select_option(user_choice)
   end
 end
