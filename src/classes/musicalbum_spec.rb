3edc8e08aa702dd5bed85e5daf22b0a18c86f211
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-10 18:43:15 -0500

    Add Music album

---
+++ src/classes/musicalbum_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'musicalbum'
+require_relative 'items'
+require 'rspec'
+
+
+
+RSpec.describe MusicAlbum do
+  describe '#initialize' do
+    it 'intilize on_spotify' do
+      musicalbum = MusicAlbum.new(false, '2022-02-01')
+      expect(musicalbum.on_spotify).to eq(false)
+    end
+  end
+  describe 'can be archive method' do
+    it 'can be archive the music album' do
+      musicalbum = MusicAlbum.new(true, '2022-02-03')
+      result = musicalbum.send(:can_be_archived?)
+      expect(result).to eq(true)
+    end
+  end
+end
