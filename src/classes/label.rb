b7e78466bcf32d7387a282f634027a5dfa52a7b5
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-11 01:23:58 +0500

    Create setter and getter for title and author

--- src/classes/label.rb
+++ src/classes/label.rb
@@ -1,6 +1,8 @@
 require_relative '../items'
 
 class Label
+  attr_accessor :title, :author
+
   def initialize(title, color, id: nil)
     @id = id.nil? ? Random.rand(1..1000) : id
     @title = title
