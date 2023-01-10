6a8dbbe89ddc66d1f6d1babec5b87c7a397dbe66
cvalencia1991 <cesar4a6z@gmail.com>
2023-01-09 20:09:17 -0500

    Add music Album class

---
+++ MusicAlbum/musicalbum.rb
@@ -0,0 +1,17 @@
+include '../items'
+
+class MusicAlbum
+  attr_reader :archived
+
+  def initialize(on_spotify: true)
+    @on_spotify = on_spotify
+    super(archived)
+  end
+
+  private
+
+  def can_be_archived?
+    true if @archived == true && @on_spotify == true
+    false
+  end
+end
