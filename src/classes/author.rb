9370425d17a3704d869cefd0801236d7a9a0a366
shakerAbuDrais <thecoffeiner@gmail.com>
2023-01-11 22:31:37 +0300

    Fix rubocop

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -1,16 +1,15 @@
 class Author
-    attr_accessor :first_name, :last_name, :id, :item
-  
-    def initialize(first_name, last_name, id)
-      @first_name = first_name
-      @last_name = last_name
-      @id = id
-      @items = []
-    end
-  
-    def add_item(item)
-      @items.push(item) unless @items.include?(item)
-      item.author = self
-    end
+  attr_accessor :first_name, :last_name, :id, :item
+
+  def initialize(first_name, last_name, id)
+    @first_name = first_name
+    @last_name = last_name
+    @id = id
+    @items = []
   end
-  
+
+  def add_item(item)
+    @items.push(item) unless @items.include?(item)
+    item.author = self
+  end
+end
