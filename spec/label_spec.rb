e8040836228b5ec619d453040e031ac2c61bfd07
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-11 21:58:47 +0500

    Add tests for add_item method in Label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -4,6 +4,7 @@ describe Label do
 
   before :each do
     @label = Label.new('Fundamental of Physics', 'cyan', id: rand(1..1000))
+    @item = Item.new(1, '2023/01/12')
   end
 
   context 'When testing a Label class' do
@@ -13,6 +14,21 @@ describe Label do
 
     it 'should return the correct title' do
       expect(@label.title).to eql 'Fundamental of Physics'
+    end
+
+    it 'should return 1 when first item is added to items array' do
+      @label.add_item(@item)
+      expect(@label.items.length).to eql 1
+    end
+    
+    it 'should add item to the items aray' do
+      @label.add_item(@item)
+      expect(@label.items[0]).to eql @item
+    end
+
+    it "should sets the item's label property to the label" do
+      @label.add_item(@item)
+      expect(@item.label).to eql @label
     end
   end
 end
--- src/classes/label.rb
+++ src/classes/label.rb
@@ -1,7 +1,7 @@
 require_relative '../../items.rb'
 
 class Label
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :items
 
   def initialize(title, color, id: nil)
     @id = id.nil? ? Random.rand(1..1000) : id
