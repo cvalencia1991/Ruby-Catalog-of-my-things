8ab0a0f2ae9041ec4d3601847d67343e6d1cd271
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-11 20:54:32 +0500

    Test move_to_archive method from Book class

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,7 +5,7 @@ describe Book do
 
   before :each do
     @book = Book.new('2000/01/01', 'Wiley', 'good', archived: true, id: rand(1..1000))
-    @book1 = Book.new('2010/01/11', 'Cole', 'cover', archived: false, id: rand(1..1000))
+    @book1 = Book.new('2010/01/11', 'Cole', 'cover', id: rand(1..1000))
     @book2 = Book.new('000/01/11', 'Cole', 'bad',  id: rand(1..1000))
   end
 
@@ -20,6 +20,16 @@ describe Book do
 
     it 'should return the correct cover_state' do
       expect(@book.cover_state).to eql 'good'
+    end
+    
+    it 'should return true when cover state is not bad or not archived' do 
+      @book1.move_to_archive
+      expect(@book1.archived).to be true
+    end
+
+    it 'should return true when the cover_state is bad or not archived' do
+      @book2.move_to_archive
+      expect(@book2.archived).to be true
     end
   end
 end
