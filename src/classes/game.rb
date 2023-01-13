df4efb0eb1d74515160f80751c4a293c349c6d16
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-13 23:39:31 +0500

    Update JSON addition for game.rb

--- src/classes/game.rb
+++ src/classes/game.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'json'
 
 class Game < Item
   attr_accessor :multiplayer, :last_played_date, :game_name
@@ -15,5 +16,26 @@ class Game < Item
   def can_be_archived?
     current_date = Date.today
     super && (current_date.year - @last_played_date.year) > 2
+  end
+
+  public
+
+  def as_json()
+    {
+      JSON.create_id => self.class.name,
+      'on_spotify' => @on_spotify,
+      'album_name' => @album_name,
+      'date' => @publish_date,
+      'archived' => @archived,
+      'id' => @id
+    }
+  end
+
+  def to_json(*options)
+    as_json.to_json(*options)
+  end
+
+  def self.json_create(object)
+    album = new(object['on_spotify'], object['album_name'], object['date'], archived: object['archived'], id: object['id'])
   end
 end
