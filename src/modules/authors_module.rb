531a1db1b14d48b0bf21b5ff7baec0c9bd6d0a48
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-13 05:57:17 +0500

    Create UI for authors

---
+++ src/modules/authors_module.rb
@@ -0,0 +1,82 @@
+require 'date'
+require 'colorize'
+require_relative '../classes/author'
+
+module AuthorModule
+  def list_all_authors
+    if @authors.empty?
+      puts "No Authors to Show 🚫 Add some authors \n"
+    else
+      puts "Available Authors in the list 👥✍ \n"
+      @authors.each_with_index do |author, index|
+        print "[#{index}]: First Name: #{author.first_name} | Last Name: #{author.last_name} \n"
+      end
+    end
+  end
+
+  def create_author
+    print 'Enter the first name of the author: '
+    first_name = gets.chomp
+    print 'Enter the second name of the author: '
+    last_name = gets.chomp
+    author = Author.new(first_name, last_name)
+    @authors << author
+    author
+  end
+
+  # def select_author
+  #   list_all_authors
+  #     print "Please select an author by typing the corresponding number or "
+  #     print "type 'back' to return to the previous menu: "
+  #     input = gets.chomp
+  #     if input == 'back'
+  #       get_author
+  #     else
+  #       author = @authors[input.to_i - 1]
+  #     return author
+  #   end
+  # end
+
+  def select_author
+    list_all_authors
+    input = nil
+    while input.nil?
+      print "Please select a genre by typing the corresponding number or "
+      print "type 'back' to return to the previous menu: "
+      input = gets.chomp
+      if !valid_input?(input)
+        puts "Invalid input. Please try again."
+        input = nil
+      elsif input == 'back'
+        get_author
+      end
+    end
+    author = @authors[input.to_i - 1]
+    return author
+  end
+  
+  def valid_input?(input)
+    return true if input == 'back'
+    (0..@genres.length).include?(input.to_i)
+  end
+
+
+  def get_author
+    puts "Select an author form the following list: "
+    puts "[ 1 ] Select from existing authors"
+    puts "[ 2 ] Add a new author"
+
+    input = gets.chomp.to_i
+    while ![1, 2].include?(input)
+      puts "Invalid input, please enter 1 or 2."
+      input = gets.chomp.to_i
+    end
+
+    case input
+    when 1
+      return select_author
+    when 2
+      return create_author
+    end
+  end
+end
