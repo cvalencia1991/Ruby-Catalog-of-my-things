f38fb1d1d701ab5d563ac50776e67233e95dd159
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-13 23:43:40 +0500

    Create JSON addition for book.rb

--- src/classes/book.rb
+++ src/classes/book.rb
@@ -1,4 +1,5 @@
 require_relative './items'
+require 'json'
 
 class Book < Item
   attr_accessor :publisher, :cover_state, :title, :label
@@ -16,5 +17,27 @@ class Book < Item
 
   def can_be_archived?
     super || @cover_state == 'bad'
+  end
+
+  public
+
+  def as_json()
+    {
+      JSON.create_id => self.class.name,
+      'title' => @title,
+      'date' => @publish_date,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state,
+      'archived' => @archived,
+      'id' => @id
+    }
+  end
+
+  def to_json(*options)
+    as_json.to_json(*options)
+  end
+
+  def self.json_create(object)
+    album = new(object['title'], object['date'], object['publisher'], object['cover_state'],  archived: object['archived'], id: object['id'])
   end
 end
