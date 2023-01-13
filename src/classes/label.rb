b74c3d4bed438a02b1621f4f5c6461b0db2350b6
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-13 05:56:50 +0500

    Create UI for labels

--- src/classes/label.rb
+++ src/classes/label.rb
@@ -1,7 +1,7 @@
 require_relative './items'
 
 class Label
-  attr_accessor :title, :author, :items
+  attr_accessor :title, :author, :items, :color
 
   def initialize(title, color, id: nil)
     @id = id.nil? ? Random.rand(1..1000) : id
---
+++ src/modules/label_module.rb
@@ -0,0 +1,82 @@
+require 'colorize'
+require_relative '../classes/label'
+require_relative './book_module'
+
+module LabelModule
+  def list_all_labels
+    if @labels.empty?
+      puts "No labels found in list 🚫 Add some items with labels \n"
+    else
+      puts "Available items with labels in the list 🔖 \n"
+      @labels.each_with_index do |label, index|
+        puts "[#{index}]: Title: #{label.title} | Color: #{label.color}"
+      end
+    end
+  end
+
+  def create_label
+    print 'Enter the title of label: '
+    title = gets.chomp
+    print 'Enter the color of the label: '
+    color = gets.chomp
+    label = Label.new(title, color)
+    @labels << label
+    return label
+  end
+
+  def select_label
+    list_all_labels
+      print 'Please select a label by typing the corresponding number or '
+      print "type 'back' to return to the previous menu: \n"
+      input = gets.chomp
+      if input == 'back'
+        get_book_label
+      else
+        label = @labels[input.to_i - 1]
+      return label
+    end
+  end
+
+
+  def select_label
+    list_all_labels
+    input = nil
+    while input.nil?
+      print 'Please select a genre by typing the corresponding number or '
+      print "type 'back' to return to the previous menu: \n"
+      input = gets.chomp
+      if !valid_input?(input)
+        puts "Invalid input. Please try again \n"
+        input = nil
+      elsif input == 'back'
+        get_book_label
+      end
+    end
+    label = @labels[input.to_i - 1]
+    return label
+  end
+  
+  def valid_input?(input)
+    return true if input == 'back'
+    (0..@genres.length).include?(input.to_i)
+  end
+
+  def get_book_label
+    puts 'Select a label for this book or create a new one: '
+    puts '[ 1 ] Select from existing labels'
+    puts '[ 2 ] Create a new label'
+
+    input = gets.chomp.to_i
+    until ![1, 2].include?(input)
+      puts "Invalid input, please enter 1 or 2 \n"
+      input = gets.chomp.to_i
+    end
+
+    case input
+    when 1
+      select_label
+    when 2
+      create_label
+    end
+  end
+end
