90711533d1b6c56cf3696eaa0fd670a6d4fe5b45
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-14 01:21:19 +0500

    Add id parameter in authors.rb

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -3,7 +3,8 @@ require 'json'
 class Author
   attr_accessor :first_name, :last_name, :id, :items
 
-  def initialize(first_name, last_name)
+  def initialize(first_name, last_name, id: nil)
+    @id = id.nil? ? Random.rand(1..1000) : id
     @first_name = first_name
     @last_name = last_name
     @items = []
