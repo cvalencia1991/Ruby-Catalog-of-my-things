2028652f297744a13a20aff95cb44f8c6c523a8e
KanzaTahreem <tehreemmsk411@gmail.com>
2023-01-13 23:46:52 +0500

    Update JSON addition for author

--- src/classes/author.rb
+++ src/classes/author.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Author
   attr_accessor :first_name, :last_name, :id, :items
 
@@ -10,5 +12,21 @@ class Author
   def add_item(item)
     @items << item
     item.author = self
+  end
+
+  def as_json()
+    {
+      JSON.create_id => self.class.name,
+      'first_name' => @first_name,
+      'last_name' => @last_name,
+    }
+  end
+
+  def to_json(*options)
+    as_json.to_json(*options)
+  end
+
+  def self.json_create(object)
+    new(object['first_name'], object['last_name'])
   end
 end
